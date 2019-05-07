c77404ecab4da981006d15a58066dfa3bedbbe41
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-07 18:30:35 +0000

    filling out template

--- TinyApp/express_server.js
+++ TinyApp/express_server.js
@@ -2,15 +2,32 @@ var express = require("express");
 var app = express();
 var PORT = 8080; 
 
-var urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+// var urlDatabase = {
+//   "b2xVn2": "http://www.lighthouselabs.ca",
+//   "9sm5xK": "http://www.google.com"
+// };
+
+
+var urlDatabase = [
+    {long: "http://www.lighthouselabs.ca", short: "#"},
+    {long: "http://www.google.com", short: "#"}
+];
+  
+
+
+app.get("/urls", (req, res) => {
+    let templateVars = { urls: urlDatabase };
+    res.render("urls_index", templateVars);
+  });
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Hello!");
+    
+    let templateVars = { urls: urlDatabase };
+ 
+    res.render("urls_index", templateVars);
+    
 });
 
 app.get("/urls.json", (req, res) => {
