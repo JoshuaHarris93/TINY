94985b60df4a1c264d5889487887331b30606594
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-09 02:22:46 +0000

    buttons working but not totally sure how..

--- TinyApp/express_server.js
+++ TinyApp/express_server.js
@@ -7,28 +7,50 @@ function generateRandomString() {
   return randomize;
 }
 
-// urlDatabaseObject = {
-//     bob: "http://google.com",
-//     potato:"https://motherfuckingwebsite.com"
-// }
+function updateURL(short, long) {
+  return urlDatabase[short] = long;
+}
+
 
 let urlDatabase = {
   h3jk3n: "http://www.lighthouselabs.ca",
   l3j4jj: "http://google.com"
 };
 
+// var keyNameWahtever = 'a new key here'
+// urlDatabase[keyNameWahtever] = 'my new value'
+// let urlDatabase = {
+//   h3jk3n: "http://www.lighthouselabs.ca",
+//   l3j4jj: "http://google.com",
+//   'a new key here': 'my new value'
+// };
+
+app.set("view engine", "ejs");
+
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.post("/urls", (req, res) => {
-//     console.log(req.body);  // Log the POST request body to the console
-//     res.send("Ok");         // Respond with 'Ok' (we will replace this)
-//   });
+app.post("/urls", (req, res) => {
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = req.body.longURL;
+  res.redirect('/urls');
+  });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
     delete urlDatabase[req.params.shortURL];
         res.redirect('/urls');
     
+});
+
+app.post("/urls/:shortURL/update", (req, res) => {
+  //extract short URL from URL path
+  //extract longURL from form submission(after submit) hint: body-parser
+  //create function that will update your URL
+  let shortURL = req.params.shortURL;
+  let longURL = req.body.longURL;
+  updateURL(shortURL, longURL);
+  // res.redirect('/urls', longURL);
+  res.redirect("/urls");
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -47,11 +69,11 @@ app.get("/urls/:shortURL", (req, res) => {
   // console.log(urlDatabase[req.params.shortURL])
   const shortURL = req.params.shortURL;
   let templateVars = {
-    shortURL: req.params.shortURL,
+    shortURL: shortURL,
     longURL: urlDatabase[shortURL]
   };
   res.render("urls_show", templateVars);
-  // res.send("OK")
+  
 });
 
 app.get("/urls", (req, res) => {
@@ -59,7 +81,6 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
-app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   let templateVars = { urls: urlDatabase };
