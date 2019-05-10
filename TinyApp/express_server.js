c94a1c11b9ce31e37e2003ee2d201996003fc523
JoshuaHarris93 <josh@emptychairproductions.com>
2019-05-10 03:02:09 +0000

    added login and register buttons but arent appearing properly

--- TinyApp/express_server.js
+++ TinyApp/express_server.js
@@ -3,23 +3,34 @@ const app = express();
 const PORT = 8080;
 
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
 
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 function generateRandomString() {
-  let randomize = Math.random().toString(36).substring(7);
+  let randomize = Math.random()
+    .toString(36)
+    .substring(7);
   return randomize;
 }
 
 function updateURL(short, long) {
-  return urlDatabase[short] = long;
+  return (urlDatabase[short] = long);
 }
-//search cookie-parser logout
-// 
 
-// DESTRUCTING EX: const {name, email, password} = req.body;
+let users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+};
 
 let urlDatabase = {
   h3jk3n: "http://www.lighthouselabs.ca",
@@ -38,25 +49,50 @@ app.set("view engine", "ejs");
 
 app.post("/login", (req, res) => {
   let chocChip = req.body.username;
-  res.cookie('username', chocChip);
-  res.redirect('/urls');
+  res.cookie("username", chocChip);
+  res.redirect("/urls");
+});
+
+app.post("/register", (req, res) => {
+  let id = generateRandomString();
+  let { email, password } = req.body;
+
+  if (!email || !password) {
+    res.redirect(400);
+  }
+
+  for (let user in users) {
+    if (email === user.email) {
+      res.redirect(400);
+    } 
+  }
+
+  if (email && password) {
+    let obj = {
+      id: id,
+      email: email,
+      password: password
+    };
+    users[id] = obj;
+    res.cookie("user_id", id);
+  }
+  res.redirect("/urls");
 });
 
 app.post("/logout", (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/urls');
+  res.clearCookie("username");
+  res.redirect("/urls");
 });
 
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL;
-  res.redirect('/urls');
-  });
+  res.redirect("/urls");
+});
 
 app.post("/urls/:shortURL/delete", (req, res) => {
-    delete urlDatabase[req.params.shortURL];
-        res.redirect('/urls');
-    
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
 
 app.post("/urls/:shortURL/update", (req, res) => {
@@ -70,17 +106,30 @@ app.post("/urls/:shortURL/update", (req, res) => {
   res.redirect("/urls");
 });
 
-app.get("/u/:shortURL", (req, res) => {
+app.get("/login", (req, res) => {
+  let templateVars = {
+    userObj: users[req.cookies["user_id"]]
+  };
+  res.render("urls_login", templateVars);
+  res.redirect("/urls");
+})
 
-  const longURL = urlDatabase[req.params.shortURL]
+app.get("/register", (req, res) => {
+  let templateVars = {
+    userObj: users[req.cookies["user_id"]]
+  };
+  res.render("urls_registration");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
   console.log(req.params);
   res.redirect(longURL);
-
 });
 
 app.get("/urls/new", (req, res) => {
   let templateVars = {
-    username: req.cookies["username"]
+    userObj: users[req.cookies["user_id"]]
   };
   res.render("urls_new", templateVars);
 });
@@ -89,27 +138,25 @@ app.get("/urls/:shortURL", (req, res) => {
   // console.log(urlDatabase[req.params.shortURL])
   const shortURL = req.params.shortURL;
   let templateVars = {
-    username: req.cookies["username"],
+    userObj: users[req.cookies["user_id"]],
     shortURL: shortURL,
     longURL: urlDatabase[shortURL]
   };
   res.render("urls_show", templateVars);
-  
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { 
-    username: req.cookies["username"],
-    urls: urlDatabase 
+  let templateVars = {
+    userObj: users[req.cookies["user_id"]],
+    urls: urlDatabase
   };
   res.render("urls_index", templateVars);
 });
 
-
 app.get("/", (req, res) => {
-  let templateVars = { 
-    username: req.cookies["username"],
-    urls: urlDatabase 
+  let templateVars = {
+    username: req.cookies["user_id"],
+    urls: urlDatabase
   };
 
   res.render("urls_index", templateVars);
@@ -120,7 +167,6 @@ app.get("/urls.json", (req, res) => {
 });
 
 // delete shortURL;
-
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
